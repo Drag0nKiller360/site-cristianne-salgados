0d7ad45909e1d6b3f592bbc60d63d75777ee9c0c
DanCodificaMT <dcsantos1503@gmail.com>
2024-11-23 22:23:34 -0300

    Tornando funcional a opção de adicionar itens no menu, atualizando automaticamente a barra lateral com o pedido.

--- js/menu.js
+++ js/menu.js
@@ -38,3 +38,32 @@ document.querySelectorAll('.card').forEach(card =>{
 document.querySelector('.preview span').onclick =  () =>{
     document.querySelector('.products-preview').style.display = 'none';
 }
+
+function adicionarItem() {
+    const div1 = document.getElementById("itens_adicionados");
+    const div2 = document.createElement("div");
+    const nome = document.getElementById("nome_item").textContent;
+    const preco_txt = document.getElementById("preco_item").textContent;
+    const inputValue = document.getElementById("item_qtd").value;
+    const preco_num = parseFloat(preco_txt.replace("R$", "").replace(",", "."));
+    const valor = Number(inputValue) * Number(preco_num);
+    div2.className = "order-item d-flex justify-content-between align-items-center mb-2";
+
+
+    div2.innerHTML = 
+    `
+        <span>${nome}</span>
+        <div class="quantity-container">
+            <p class="label">QUANTIDADE</p>
+            <div class="number-input">
+                <button type="button" onclick="this.parentNode.querySelector('input[type=number]').stepDown()"></button>
+                <input class="quantity" min="0" name="quantity" value="${inputValue}" type="number">
+                <button type="button" onclick="this.parentNode.querySelector('input[type=number]').stepUp()" class="plus"></button>
+            </div>
+        </div>
+        <span>R$${valor.toFixed(2).replace(".",",")}</span>
+    `
+    ;
+
+    div1.appendChild(div2);
+}
