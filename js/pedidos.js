3bbc987b1ea76095ab73adc454514480e12ae52c
Joadson Ferreira do Nascimento <joadsonferreira8@gmail.com>
2024-11-24 18:12:44 -0300

    Criação das Popups de Pagamento.

--- js/pedidos.js
+++ js/pedidos.js
@@ -2,3 +2,21 @@
 function redirecionar(pagina) {
     window.location.href = pagina;
 }
+
+// Função para abrir as pop-ups
+function openPopup(type) {
+    document.getElementById('popup-overlay').style.display = 'block';
+    document.getElementById(`popup-${type}`).style.display = 'block';
+}
+
+// Função para fechar as pop-ups
+function closePopup() {
+    document.getElementById('popup-overlay').style.display = 'none';
+    document.querySelectorAll('.popup').forEach(popup => popup.style.display = 'none');
+}
+
+// Função para finalizar o pedido
+function finalizeOrder() {
+    alert('Pedido finalizado com sucesso!');
+    closePopup();
+}
