5e4ce368b3f3c92fcf1f0534e9e9222de413fdd1
Paulo Sérgio da Silva Júnior <paulo.ss.junior123@gmail.com>
2024-11-23 21:20:53 -0300

    Alteração no sistema de pop-up, agora é possível clicar em qualquer parte do produto pra abrir a pop up com imagem, nome e preço coletado

--- js/menu.js
+++ js/menu.js
@@ -22,11 +22,17 @@ document.querySelector('.btn-group').addEventListener('click', function(e) {
     }
 });
 
-document.querySelectorAll('.card img').forEach(image =>{
-    image.onclick = () =>{
+document.querySelectorAll('.card').forEach(card =>{
+    card.addEventListener('click', function(event) {
+        const img = event.target.closest('.card').querySelector('img');
+        const nome = event.target.closest('.card').querySelector('h5');
+        const preco = event.target.closest('.card').querySelector('.preco');
+
         document.querySelector('.products-preview').style.display = 'block';
-        document.querySelector('.preview img').src = image.getAttribute('src');
-    }  
+        document.querySelector('.preview img').src = img.getAttribute('src');
+        document.querySelector('.preview h3').textContent = nome.textContent;
+        document.querySelector('.preview .preco').textContent = preco.textContent;
+    });
 })
 
 document.querySelector('.preview span').onclick =  () =>{
